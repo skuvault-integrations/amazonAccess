fabefab83faf24097f4581c80cf645fcbbc9d8f2
jeky11 <dmitriy.kholodov@gmail.com>
2015-01-03 20:31:24 +0300

    Updated tests to use external credentials

---
+++ src/AmazonAccessTests/TestConfig.cs
@@ -0,0 +1,16 @@
+﻿using LINQtoCSV;
+
+namespace AmazonAccessTests
+{
+	internal class TestConfig
+	{
+		[CsvColumn(Name = "AccessKeyId", FieldIndex = 1)]
+		public string AccessKeyId { get; set; }
+
+		[CsvColumn(Name = "SecretAccessKeyId", FieldIndex = 2)]
+		public string SecretAccessKeyId { get; set; }
+
+		[CsvColumn(Name = "SellerId", FieldIndex = 3)]
+		public string SellerId { get; set; }
+	}
+}
