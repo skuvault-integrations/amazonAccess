07800017c4a4d2c0ca19701cf33a7aa74046509c
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2016-09-21 18:35:08 +0300

    Try create for marketplaces

--- src/AmazonAccess/Models/AmazonMarketplaces.cs
+++ src/AmazonAccess/Models/AmazonMarketplaces.cs
@@ -12,39 +12,10 @@ namespace AmazonAccess.Models
 		public string FbaInventoryServiceUrl{ get; private set; }
 		public string FeedsServiceUrl{ get; private set; }
 		public string SellersServiceUrl{ get; private set; }
-		public readonly List< AmazonMarketplace > Marketplaces = new List< AmazonMarketplace >();
+		public List< AmazonMarketplace > Marketplaces{ get; private set; }
 
-		public AmazonMarketplaces( string countryCode )
-			: this( new AmazonMarketplace( countryCode ) )
+		private void Init( List< AmazonMarketplace > marketplaces )
 		{
-		}
-
-		public AmazonMarketplaces( List< string > countryCodes )
-			: this( countryCodes.Select( x => new AmazonMarketplace( x ) ).ToList() )
-		{
-		}
-
-		public AmazonMarketplaces( AmazonMarketplace marketplace )
-		{
-			Condition.Requires( marketplace, "marketplace" ).IsNotNull();
-
-			this.RegionCode = marketplace.RegionCode;
-			this.OrdersServiceUrl = marketplace.OrdersServiceUrl;
-			this.ProductsServiceUrl = marketplace.ProductsServiceUrl;
-			this.FbaInventoryServiceUrl = marketplace.FbaInventoryServiceUrl;
-			this.FeedsServiceUrl = marketplace.FeedsServiceUrl;
-			this.SellersServiceUrl = marketplace.SellersServiceUrl;
-			this.Marketplaces.Add( marketplace );
-		}
-
-		public AmazonMarketplaces( List< AmazonMarketplace > marketplaces, bool ignoreNonAmazonMarketplaces = false )
-		{
-			Condition.Requires( marketplaces, "marketplaces" ).IsNotNull().IsNotEmpty();
-			var regionsCount = marketplaces.GroupBy( x => x.RegionCode ).Count();
-			Condition.Requires( regionsCount, "marketplaces" ).IsEqualTo( 1, "Found marketplaces from different regions" );
-			var duplicates = marketplaces.GroupBy( x => x.MarketplaceId ).Any( x => x.Count() > 1 );
-			Condition.Requires( duplicates, "marketplaces" ).IsEqualTo( false, "Found duplicates of marketplaces" );
-
 			var marketplace = marketplaces.First();
 			this.RegionCode = marketplace.RegionCode;
 			this.OrdersServiceUrl = marketplace.OrdersServiceUrl;
@@ -52,7 +23,66 @@ namespace AmazonAccess.Models
 			this.FbaInventoryServiceUrl = marketplace.FbaInventoryServiceUrl;
 			this.FeedsServiceUrl = marketplace.FeedsServiceUrl;
 			this.SellersServiceUrl = marketplace.SellersServiceUrl;
-			this.Marketplaces.AddRange( ignoreNonAmazonMarketplaces ? marketplaces.Where( x => x.IsAmazonMarketplace ) : marketplaces );
+			this.Marketplaces = marketplaces;
+		}
+
+		private AmazonMarketplaces( List< AmazonMarketplace > marketplaces )
+		{
+			this.Init( marketplaces );
+		}
+
+		public AmazonMarketplaces( string countryCode )
+			: this( new AmazonMarketplace( countryCode ) )
+		{
+		}
+
+		public AmazonMarketplaces( AmazonMarketplace marketplace )
+		{
+			Condition.Requires( marketplace, "marketplace" ).IsNotNull();
+			this.Init( new List< AmazonMarketplace > { marketplace } );
+		}
+
+		public AmazonMarketplaces( List< string > countryCodes )
+			: this( countryCodes.Select( x => new AmazonMarketplace( x ) ).ToList(), true )
+		{
+		}
+
+		public AmazonMarketplaces( List< AmazonMarketplace > marketplaces, bool ignoreNonAmazonMarketplaces = false )
+		{
+			Condition.Requires( marketplaces, "marketplaces" ).IsNotNull();
+
+			if( ignoreNonAmazonMarketplaces )
+				marketplaces = marketplaces.Where( x => x.IsAmazonMarketplace ).ToList();
+			Condition.Requires( marketplaces, "marketplaces" ).IsNotEmpty();
+
+			var regionsCount = marketplaces.GroupBy( x => x.RegionCode ).Count();
+			Condition.Requires( regionsCount, "marketplaces" ).IsEqualTo( 1, "Found marketplaces from different regions" );
+
+			var duplicates = marketplaces.GroupBy( x => x.MarketplaceId ).Any( x => x.Count() > 1 );
+			Condition.Requires( duplicates, "marketplaces" ).IsEqualTo( false, "Found duplicates of marketplaces" );
+
+			this.Init( marketplaces );
+		}
+
+		public AmazonMarketplaces TryCreate( List< AmazonMarketplace > marketplaces, bool ignoreNonAmazonMarketplaces = false )
+		{
+			if( marketplaces == null )
+				return null;
+
+			if( ignoreNonAmazonMarketplaces )
+				marketplaces = marketplaces.Where( x => x.IsAmazonMarketplace ).ToList();
+			if( marketplaces.Count == 0 )
+				return null;
+
+			var regionsCount = marketplaces.GroupBy( x => x.RegionCode ).Count();
+			if( regionsCount != 1 )
+				return null;
+
+			var duplicates = marketplaces.GroupBy( x => x.MarketplaceId ).Any( x => x.Count() > 1 );
+			if( duplicates )
+				return null;
+
+			return new AmazonMarketplaces( marketplaces );
 		}
 
 		public List< string > GetMarketplaceIdAsList()
